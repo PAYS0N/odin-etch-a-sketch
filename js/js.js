d54632fe61431e914eecb102dd4f3f201f4eefdc
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-10-18 00:46:32 -0500

    Add input validation

--- js/js.js
+++ js/js.js
@@ -20,8 +20,9 @@ function createGrid(num) {
     }
 }   
 
-//set box continer width to its height
 const boxContainer = document.querySelector(".box-square");
+
+//set box continer width to its height
 let height = boxContainer.parentNode.clientHeight;
 let width = boxContainer.parentNode.clientWidth;
 boxContainer.style.height = Math.min(height, width) + 'px';
@@ -29,3 +30,27 @@ boxContainer.style.width = Math.min(height, width) + 'px';
 
 //create base grid
 createGrid(16);
+
+//on button click, re-generate grid
+function removeGrid() {
+    let rows = document.querySelectorAll(".row");
+
+    rows.forEach((row) => {
+        row.remove();
+    })
+}
+
+function generateNewGrid(num){
+    if (typeof num === 'number' & num > 0 & num < 100){
+        removeGrid();
+        createGrid(num);
+    }
+    else {
+        alert("Error with value, please enter a number between 0 and 100")
+    }
+}
+
+const createButton = document.querySelector(".create-button");
+const input = document.querySelector(".box-input");
+createButton.addEventListener("click", () => generateNewGrid(+input.value));
+input.value = '';
