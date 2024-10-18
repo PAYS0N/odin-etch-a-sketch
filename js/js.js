314180ae6428334e320c26c422966da9350a73dc
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-10-17 19:39:46 -0500

    Add initial cells

--- js/js.js
+++ js/js.js
@@ -1,1 +1,28 @@
+function createGrid(num) {
+    let row;
+    let cell;
+    let cellSideLength = boxContainer.clientHeight/num;
+    for (let i = 0; i < num; i++) {
+        row = document.createElement("div");
+        row.setAttribute("class", "row");
+
+        for (let j = 0; j < num; j++) {
+            cell = document.createElement("div");
+            cell.setAttribute("class", "cell");
+            cell.style.height = cellSideLength + 'px';
+            cell.style.width = cellSideLength + 'px';                        
+            row.appendChild(cell);
+        }
+        boxContainer.appendChild(row);
+    }
+}
+
 //set box continer width to its height
+const boxContainer = document.querySelector(".box-square");
+let height = boxContainer.parentNode.clientHeight;
+let width = boxContainer.parentNode.clientWidth;
+boxContainer.style.height = Math.min(height, width) + 'px';
+boxContainer.style.width = Math.min(height, width) + 'px';
+
+//create base grid
+createGrid(16);
